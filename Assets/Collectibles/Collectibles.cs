1e87ffa7cbdb0b4c28af33d7cdd93db030ce3c2f
Mathilde Cellier <mathoche98@gmail.com>
2020-10-23 18:01:39 +0200

    Beginning of the project
    Create files and elements of the game

---
+++ Assets/Collectibles/Collectibles.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collectibles : MonoBehaviour
+{
+    // variables
+    //score apearing in the inspector
+    [SerializeField] private Text Score ;
+    private int scoreValue ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // at the beggining the score is 0
+        scoreValue = 0 ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // apearing score and its value on the sreen
+        Score.text = "Score : " + scoreValue ;
+
+    }
+
+    // call the fonction to detect if objects are collided
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // message in the console
+        Debug.Log("+1") ;
+        //
+        if (other.gameObject.CompareTag("Collectible"))
+        {
+            Destroy(other.gameObject) ;
+            //scoreValue += 1 ;
+        }
+    }
+}
---
+++ Assets/PlaySceneLoading.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlaySceneLoading : MonoBehaviour
+{
+    [SerializeField] public Animator transition; 
+
+    [SerializeField] public float transitionTime = 1f;
+
+    public void LoadScene()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+        
+}
