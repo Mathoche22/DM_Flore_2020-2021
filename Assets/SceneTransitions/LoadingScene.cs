0435ef501c3eda6cbc95004c530201919c8afdf4
Mathilde Cellier <mathilde.cellier1@free.fr>
2020-10-30 23:15:47 +0100

    Start Loading scene

--- Assets/SceneTransitions/LoadingScene.cs
+++ Assets/SceneTransitions/LoadingScene.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadingScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Animator animator;
+    public float transitionTime = 1.5f;
+    [SerializeField] public int NextScene;
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadNextScene()
     {
-        
+        NextScene = SceneManager.GetActiveScene().buildIndex + 1; 
+        StartCoroutine(LoadingScene(NextScene));
+    }
+
+    IEnumerator LoadingScene(int buil_index)
+    {
+        animaor.SetTrigger("EndLoad");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadingScene(buil_index);
     }
 }
