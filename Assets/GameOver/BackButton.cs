ec62c412368fd0b1daccbf2521c6c6adc73a9dca
Mathilde Cellier <mathilde.cellier1@free.fr>
2020-10-30 23:36:07 +0100

    back to Menu from game over scene

---
+++ Assets/GameOver/BackButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButton : MonoBehaviour
+{
+    // call a function to go back
+    public void GoBack()
+    {
+        // call the Menu scene
+        SceneManager.LoadScene("MENU");
+    }
+}
--- Assets/SceneTransitions/LoadingScene.cs
+++ Assets/SceneTransitions/LoadingScene.cs
@@ -5,25 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class LoadingScene : MonoBehaviour
 {
+    // get the animations
     Animator animator;
+    // set a time for the transition
     public float transitionTime = 1.5f;
+    // the next scene can be changed in the inspector
     [SerializeField] public int NextScene;
 
     private void Awake()
     {
+        // get a component on the animation
         animator = GetComponent<Animator>();
     }
 
-    public void LoadNextScene()
+    // function to call the transition scene animation
+    public void LoadScene()
     {
+        // apply a scene to call
         NextScene = SceneManager.GetActiveScene().buildIndex + 1; 
-        StartCoroutine(LoadingScene(NextScene));
+        // call the next scene
+        StartCoroutine(LoadScene(NextScene));
     }
 
-    IEnumerator LoadingScene(int buil_index)
+    IEnumerator LoadScene(int buil_index)
     {
-        animaor.SetTrigger("EndLoad");
+        animator.SetTrigger("EndLoad");
         yield return new WaitForSeconds(transitionTime);
-        SceneManager.LoadingScene(buil_index);
+        SceneManager.LoadScene(buil_index);
     }
+
 }
