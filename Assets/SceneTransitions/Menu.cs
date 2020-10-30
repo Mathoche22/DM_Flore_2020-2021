f4131295071dfef0cb75ffa2682b0cb0590007f8
Mathilde Cellier <mathilde.cellier1@free.fr>
2020-10-30 23:42:30 +0100

    Update menu script

--- Assets/SceneTransitions/Menu.cs
+++ Assets/SceneTransitions/Menu.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    // create a fonction fo each button
+
     public void PlayButton()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // call the scene to load
+        SceneManager.LoadScene("PLAY");
+    }
+
+    public void CreditButton()
+    {
+        // call the scene to load
+        SceneManager.LoadScene("CREDITS");
     }
 
     public void QuitButton()
     {
+        // call Tthe function that quit the game
         Application.Quit();
     }
 }
